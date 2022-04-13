4db90e08910923e8ed072fc19ac754ce3cebcbe1
Arsenty Halimovsky <41345907+Hieronius@users.noreply.github.com>
2022-04-13 15:13:34 +0300

    Right on target v1.3

---
+++ Right on target v1.3/Right on target/Model/GameRound.swift
@@ -0,0 +1,31 @@
+
+
+import Foundation
+
+protocol GameRoundProtocol {
+    // Количество разработанных за раунд очков
+    var score: Int { get }
+    // Загаданное значение
+    var currentSecretValue: Int { get }
+    // Подсчет заработанных за раунд очков
+    func calculateScore(with value: Int)
+}
+
+class GameRound: GameRoundProtocol {
+    var score: Int = 0
+    var currentSecretValue: Int = 0
+    init(secretValue: Int) {
+        currentSecretValue = secretValue
+    }
+    
+    // Подсчитывает количество очков
+    func calculateScore(with value: Int) {
+        if value > currentSecretValue {
+            score = 50 - value + currentSecretValue
+        } else if value < currentSecretValue {
+            score = 50 - currentSecretValue + value
+        } else {
+            score = 50
+        }
+    }
+}

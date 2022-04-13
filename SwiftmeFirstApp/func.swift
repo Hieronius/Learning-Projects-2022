691492987d6ba24274390e9ecc2b71abd9915087
Arsenty Halimovsky <ghost12@yandex.ru>
2022-04-13 15:42:50 +0300

    NewUpdate1
    Test update

---
+++ SwiftmeFirstApp/func.swift
@@ -0,0 +1,10 @@
+//
+//  func.swift
+//  SwiftmeFirstApp
+//
+//  Created by Арсентий Халимовский on 24.01.2022.
+//
+
+func sum(_ a: String?, _ b: String?) -> Int {
+    return Int(a!)! + Int(b!)!
+}

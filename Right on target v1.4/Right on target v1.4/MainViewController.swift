599bff0077a7b74ec55a2190f358d8f00b3cd641
Arsenty Halimovsky <ghost12@yandex.ru>
2022-04-19 05:00:04 +0300

    Adding new version

---
+++ Right on target v1.4/Right on target v1.4/MainViewController.swift
@@ -0,0 +1,16 @@
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    // MARK: - Переходы
+    
+    @IBAction func previousScreen( unwindSegue: UIStoryboardSegue ) {
+        return
+    }
+}
---
+++ Right on target v1.4/Right on target v1.4/Model/Game v1.4.swift
@@ -0,0 +1,70 @@
+// Сущность игра для работы с цветом
+
+/// Протокол, определяющий требования к классу Game
+
+protocol GameProtocol {
+    
+    associatedtype SecretType
+    
+    // Количество заработанных очков
+    var score: Int { get }
+    
+    // Секретное число
+    var secretValue: SecretType { get }
+    
+    // Проверяет окончена ли игра
+    var isGameEnded: Bool { get }
+    
+    // Начинает новую игру и сразу стартует первый раунд
+    func restartGame()
+    
+    // Начинает новый раунд
+    func startNewRound()
+    
+    // Сравнение значения пользователя с загаданным и подсчет очков
+    func calculateScore(secretValue: SecretType, userValue: SecretType)
+}
+
+class Game<T: SecretValueProtocol>: GameProtocol {
+    typealias SecretType = T
+    
+    var score: Int = 0
+    
+    // Секретное значение
+    var secretValue: T
+    
+    // Замыкание производит сравнение значений и возвращает заработанные очки
+    private var compareClosure: (T, T) -> Int
+    private var roundsCount: Int!
+    private var currentRoundNumber: Int = 0
+    
+    var isGameEnded: Bool {
+        if currentRoundNumber == roundsCount {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    init(secretValue: T, rounds: Int, compareClosure: @escaping (T, T) -> Int) {
+        self.secretValue = secretValue
+        roundsCount = rounds
+        self.compareClosure = compareClosure
+        startNewRound()
+    }
+    
+    func restartGame() {
+        score = 0
+        currentRoundNumber = 0
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        currentRoundNumber += 1
+        self.secretValue.setRandomValue()
+    }
+    
+    func calculateScore(secretValue: T, userValue: T) {
+        score = score + compareClosure(secretValue, userValue)
+    }
+}

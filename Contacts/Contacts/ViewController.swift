812b4fbef9e76be5db2d1f1fe38f7dce99d6fcb9
Arsenty Halimovsky <ghost12@yandex.ru>
2022-04-25 06:35:23 +0300

    New Method

--- Contacts/Contacts/ViewController.swift
+++ Contacts/Contacts/ViewController.swift
@@ -17,3 +17,36 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    // Функция возвращающая общее число ячеек в таблице
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    // Функция возвращающая конкретный экземпляр ячейки для конкретной строки таблицы
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // производим попытку загрузки переиспользованной ячейки
+        guard var cell = tableView.dequeueReusableCell(withIdentifier: "MyCell")
+    else {
+        
+        print("Создаем новую ячейку для строки с индексом \(indexPath.row)")
+        
+        // получаем экземпляр ячейки
+        var newCell = UITableViewCell(style: .default, reuseIdentifier: "MyCell")
+        configure(cell: &newCell, for: indexPath)
+        return newCell
+    }
+        // возвращаем сконфигурированный экземпляр ячейки
+        print("Используем старуя ячейку для строки с индексом \(indexPath.row)")
+        configure(cell: &cell, for: indexPath)
+        return cell
+    }
+    
+    private func configure(cell: inout UITableViewCell, for indexPath: IndexPath) {
+        var configuration = cell.defaultContentConfiguration()
+        configuration.text = "Строка \(indexPath.row)"
+        cell.contentConfiguration = configuration
+    }
+    
+}
+

a69085bbe8573362c1da2dbe7ada4180b4a9938f
Arsenty Halimovsky <ghost12@yandex.ru>
2022-04-27 11:23:45 +0300

    Updating House app

--- Square Area/Square Area/main.swift
+++ Square Area/Square Area/main.swift
@@ -3,24 +3,37 @@ import Foundation
 
 class House {
     
-    var width: Double
-    var height: Double
+    var width: String?
+    var height: String?
     
     func build() {
-        print("area is - \(width * height)")
+        print("area is - \(Int(width!)! * Int(height!)!) ")
     }
     
     func getWidth() {
-        print("Width is - \(width)")
+        print("Width is - \(width!)")
     }
     
     func getHeight() {
-        print("Height is - \(height)")
+        print("Height is - \(height!)")
     }
     
-    init(width: Double, height: Double) {
+    init(width: String?, height: String?) {
         self.width = width
         self.height = height
     }
 }
+
+print("Введите ширину")
+var a = readLine()
+print("Введите высоту")
+var b = readLine()
+
+var objectSquare = House(width: a, height: b)
+
+print("Характеристики дома такие:")
+objectSquare.build()
+objectSquare.getWidth()
+objectSquare.getHeight()
+
 

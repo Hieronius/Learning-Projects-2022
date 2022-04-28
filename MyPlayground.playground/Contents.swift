bdcc9afd2b97b94fbbc13535219ff40efa68166c
Arsenty Halimovsky <ghost12@yandex.ru>
2022-04-28 12:00:19 +0300

    New completed tasks

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class Students {
+    var arrayOfStudents: [String]
+    
+    func arrayOfStudentsSort() -> ([String], Int) {
+        var counter = 0
+        for student in arrayOfStudents {
+            counter += 1
+            if counter >= 30 {
+                print("There is no more space for new students")
+            } else {
+                print("Welcome to our class, summoner!")
+            }
+        }
+        return (arrayOfStudents.sorted {$0 < $1}, counter)
+    }
+    
+    init(arrayOfStudents: [String]) {
+        self.arrayOfStudents = arrayOfStudents
+    }
+}
+
+var classOfStudents = Students(arrayOfStudents: ["Darvin", "Jalla", "Kenzo", "Marla", "Antonidas"])
+classOfStudents.arrayOfStudentsSort()

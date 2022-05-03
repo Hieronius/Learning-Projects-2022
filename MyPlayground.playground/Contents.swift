312e5761366dcd501b7c8fd096c29f14db747a7d
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-03 11:03:28 +0300

    Daily practice 03.05.2022.
    Trying to build program that will add new purchase in our "bag" and print: "product is bought"

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,103 +1,28 @@
-// 5-10 structs
+// List of purchases
 
-struct Car {
-    var price: Int
-    var weight: Int
-    var yearOfProduce: Int
-    var name: String
-    var engine: Double
+
+struct ListOfPurchases {
+    
+    let bread = "White"
+    let meat = "Chicken breast"
+    let vegetables = "Cabage"
+    let chocolate = "Alpen Gold"
+    let spicy = "Pepper"
+    let iceCream = "48 coins"
+    let cereal = "Pasta"
 }
 
-struct myOutFitBag {
-    var phone = "Iphone"
-    var tablet = "Ipad"
-    var laptop = "MacBook"
-    var pen = "Apple pencil"
-    var headPhones = "Beats solo 3"
-    var player = "Iphone 6s"
-}
+let list = ListOfPurchases()
+var ourPackage = [ListOfPurchases]()
 
-struct myOutfit {
-    var uppedBody = "T-shirt"
-    var lowerBody = "Shorts"
-    var underwear = "boxer briefs"
-    var shoes = "Reebok fly"
-    var sunglasses = "hunder rubles sunglasses"
-    var assesories = ["stil chain", "stil ring", "apple watch"]
+
+func purchaseFromTheList(position: ListOfPurchases) {
+    ourPackage.append(position)
+    print("\(position) is bought")
     
 }
 
-struct myNotes {
-    var firstFolder = "Managment"
-    var secondFolder = "Self-development"
-    var thirthFolder = "Sales"
-    var fourthFolder = "Fitness"
-    var fivethFolder = "Psychology"
-}
+purchaseFromTheList(position: list)
+ourPackage
 
-struct myChar {
-    var strenght = 15
-    var intellect = 10
-    var agility = 15
-    var vitality = 25
-    var stamina = 55
-    var charisma = 25
-}
-
-struct TerranArmy {
-    var soldier = "Space marine"
-    var spy = "Ghost"
-    var commander = "Jim Rainor"
-    var machine = "Goliaf"
-    var flyMachine = "Battle Cruiser"
-    var commandCentre = "Braxis"
-}
-
-struct myResume {
-    var age: Int
-    var namy: String
-    var experience: Int
-    var specialisation: String
-    var hasCar: Bool
-    var isMarried: Bool
-    var countryFrom: String
-    var preferedLanguage: String
-    
-}
-
-struct Building {
-    var height: Int
-    var weight: Int
-    var cost: Int
-    var stages: Int
-    var location: String
-    var goodArea: Bool
-    var isMortageAvaliable: Bool
-    var parkingIncludes: Bool
-}
-
-struct Ammunition {
-    var steelArms = "Machete"
-    var pistol = "Magnum"
-    var rifle = "Kalashnikov"
-    var granade = "M22"
-    var car = "Hammer"
-    var opticalVision = "infrared sight"
-}
-
-
-struct Point: Equatable {
-    var x: Int
-}
-
-struct X {
-    var point: Point {
-        didSet { point.x = 0}
-    }
-}
-
-let y = Point(x: 5)
-let p = X(point: y)
-
-let result = p.point.x == y.x
-print(result)
+// Trying to make programm that will add new product to our bag and print "product is bought"

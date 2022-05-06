5012ca92f21643811e2fd0dc0c89c9ea936bb81b
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-06 06:38:12 +0300

    Added connection between who controllers

--- TransferApp/TransferApp/SecondViewController.swift
+++ TransferApp/TransferApp/SecondViewController.swift
@@ -2,7 +2,19 @@ import UIKit
 
 
 class SecondViewController: UIViewController {
+    
     // ...
     @IBOutlet var dataTextField: UITextField!
     
+    var updatingData: String = " "
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateTextFieldData(withText: updatingData)
+    }
+    
+    // обновляем данные в текстовом поле
+    private func updateTextFieldData(withText text: String) {
+        dataTextField.text = text
+    }
 }
--- TransferApp/TransferApp/ViewController.swift
+++ TransferApp/TransferApp/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet var dataLabel: UILabel!
+    
+    @IBAction func editDataWithProperty(_ sender: UIButton) {
+        // получаем вью контроллер, в который происходит переход
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let editScreen = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        
+        // передаем данные
+        editScreen.updatingData = dataLabel.text ?? ""
+        
+        // переходим к следующему экрану
+        self.navigationController?.pushViewController(editScreen, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()

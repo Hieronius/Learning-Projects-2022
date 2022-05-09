fe9b07ead8764d48e4edc24c374ffa5284768062
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-09 05:16:23 +0300

    Major changes
    Adding Model folder with essence of task and tasks storage

---
+++ To-Do Manager/To-Do Manager/Model/Task.swift
@@ -0,0 +1,33 @@
+// Тип задачи
+enum TaskPriority {
+    // текущая
+    case normal
+    // важная
+    case important
+}
+
+// Состояние задачи
+enum TaskStatus {
+    // запланированная
+    case planned
+    // завершенная
+    case completed
+}
+
+// Требования к типу, описывающему сущность "Задача"
+protocol TaskProtocol {
+    // название
+    var title: String { get set }
+    // тип
+    var type: TaskPriority { get set }
+    // статус
+    var status: TaskStatus { get set }
+    
+}
+
+// Сущность "Задача"
+struct Task: TaskProtocol {
+    var title: String
+    var type: TaskPriority
+    var status: TaskStatus
+}

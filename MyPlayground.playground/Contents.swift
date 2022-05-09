742ea95a898917c98dec48d18682768d97e4b0cf
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-09 15:40:35 +0300

    Practice with class Calculator

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,50 +1,34 @@
 
 
-class Bank {
-    var userFirstName: String
-    var userLastName: String
+class Calculator {
     
-    lazy var fullName: String = { () -> String in
-        userFirstName + " " + userLastName
-    }()
+    var firstNumber: Int
+    var secondNumber: Int
     
-    var currentMoney: Double {
-        willSet(newCurrentMoney) {
-            print("Было на счету \(self.currentMoney) а стало \(newCurrentMoney)")
-        }
-        didSet(oldCurrentMoney){
-            print("Сумма увеличилась на \(currentMoney - oldCurrentMoney)")
-        }
-    }
-    var typeOfMoney: String
-    
-    var currentInterestRate: Double
-    
-    var profit: Double {
-        get {
-            currentMoney * currentInterestRate
-        }
-        set(newProfit) {
-            self.currentMoney = newProfit / currentInterestRate
-        }
+    var addition: Int {
+        firstNumber + secondNumber
     }
     
-    init(userFirstName: String, userLastName: String, currentMoney: Double, typeOfMoney: String, currentInterestRate: Double) {
-        self.userFirstName = userFirstName
-        self.userLastName = userLastName
-        self.currentMoney = currentMoney
-        self.typeOfMoney = typeOfMoney
-        self.currentInterestRate = currentInterestRate
+    var subtraction: Int {
+        firstNumber - secondNumber
     }
     
+    var multiply: Int {
+        firstNumber * secondNumber
+    }
+    
+    var division: Int {
+        firstNumber / secondNumber
+    }
+    
+    init(firstNumber: Int, secondNumber: Int) {
+        self.firstNumber = firstNumber
+        self.secondNumber = secondNumber
+    }
 }
 
-var myBankAccount = Bank(userFirstName: "Arsenty", userLastName: "Khalimovsky", currentMoney: 200_000, typeOfMoney: "Rubles", currentInterestRate: 15.0)
-myBankAccount.fullName
-myBankAccount.profit
-myBankAccount.profit = 350_000
-myBankAccount.currentMoney
-
-class AlfaBank: Bank {
-    
-}
+var firstTwoNumbers = Calculator(firstNumber: 50, secondNumber: 25)
+firstTwoNumbers.addition
+firstTwoNumbers.subtraction
+firstTwoNumbers.division
+firstTwoNumbers.multiply

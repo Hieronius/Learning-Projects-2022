988868c159244b846858dea29987cd2d8773aaee
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-09 16:16:32 +0300

    Trying to make my own AppStore

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,34 +1,43 @@
 
 
-class Calculator {
+protocol AppStoreProtocol {
+    var nameOfTheApp: String { get set }
+    var appCategory: String { get set }
     
-    var firstNumber: Int
-    var secondNumber: Int
+    var arrayOfApps: [AppStoreProtocol]? { get set }
     
-    var addition: Int {
-        firstNumber + secondNumber
+    func saveApp(app: AppStoreProtocol) -> [AppStoreProtocol]?
+    func loadApp(app: AppStoreProtocol)
+    
+    
+}
+
+class AppStore: AppStoreProtocol {
+   
+    var nameOfTheApp: String
+    var appCategory: String
+    
+    var arrayOfApps: [AppStoreProtocol]?
+    
+    func saveApp(app: AppStoreProtocol) -> [AppStoreProtocol]? {
+        arrayOfApps?.append(app)
+        print("\(app.nameOfTheApp) was saved in AppStore")
+        return arrayOfApps
     }
     
-    var subtraction: Int {
-        firstNumber - secondNumber
+    func loadApp(app: AppStoreProtocol) {
+        print("\(app) was launched")
     }
     
-    var multiply: Int {
-        firstNumber * secondNumber
-    }
-    
-    var division: Int {
-        firstNumber / secondNumber
-    }
-    
-    init(firstNumber: Int, secondNumber: Int) {
-        self.firstNumber = firstNumber
-        self.secondNumber = secondNumber
+    init(nameOfTheApp: String, appCategory: String) {
+        self.nameOfTheApp = nameOfTheApp
+        self.appCategory = appCategory
     }
 }
 
-var firstTwoNumbers = Calculator(firstNumber: 50, secondNumber: 25)
-firstTwoNumbers.addition
-firstTwoNumbers.subtraction
-firstTwoNumbers.division
-firstTwoNumbers.multiply
+var toDoList = AppStore(nameOfTheApp: "To-Do list", appCategory: "SelfDevelopment")
+toDoList.saveApp(app: <#T##AppStoreProtocol#>)
+
+// Almost done with load and save func but it seems like i don't need class "AppStore". May be protocol will be enough
+
+// May be i need empty class AppStore with extension

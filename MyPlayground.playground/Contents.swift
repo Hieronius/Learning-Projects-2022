b86210d91ab56f5f35d0fd628275c1bb19c09e79
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-10 11:13:02 +0300

    Made own AppStore with structs

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,6 +1,6 @@
 
 
-protocol AppStoreProtocol {
+/* protocol AppStoreProtocol {
     var nameOfTheApp: String { get set }
     var appCategory: String { get set }
     
@@ -41,3 +41,37 @@ toDoList.saveApp(app: <#T##AppStoreProtocol#>)
 // Almost done with load and save func but it seems like i don't need class "AppStore". May be protocol will be enough
 
 // May be i need empty class AppStore with extension
+*/
+
+struct App {
+    var appName: String
+    var appCategory: String
+    
+}
+
+struct AppStore {
+    
+    var appMarket = [App]()
+    
+    mutating func save(app: App) -> [App] {
+        appMarket.append(app)
+        print("App \(app.appName) was saved to the AppMarket")
+        return appMarket
+    }
+    
+    func load(app: App) {
+        print("App \(app.appName) was loaded")
+    }
+    
+}
+
+var toDoList = App(appName: "ToDoList", appCategory: "Self-Development")
+var maps = App(appName: "Apple Maps", appCategory: "Location")
+
+var appStore = AppStore()
+appStore.save(app: toDoList)
+appStore.save(app: maps)
+appStore.appMarket
+appStore.load(app: toDoList)
+
+

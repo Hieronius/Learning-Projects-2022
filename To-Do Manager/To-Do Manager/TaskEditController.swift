325b847592fcdeee4f259f6b6f2bdd7242d38486
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-13 06:00:42 +0300

    Adding buttons for new scene

--- To-Do Manager/To-Do Manager/TaskEditController.swift
+++ To-Do Manager/To-Do Manager/TaskEditController.swift
@@ -2,10 +2,35 @@ import UIKit
 
 class TaskEditController: UITableViewController {
     
+    // Название типов задач
+    private var taskTitles: [TaskPriority: String] = [
+        .important: "Важная",
+        .normal: "Текущая"]
+    
+    @IBOutlet var taskTypeLabel: UILabel!
+    
+    @IBOutlet var taskTitle: UITextField!
     
     var doAfterEdit: ((String, TaskPriority, TaskStatus) -> Void?)
+    
     // Параметры задачи
     var taskText = " "
     var taskType: TaskPriority = .normal
     var taskStatus: TaskStatus = .planned
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // обновление текстового поля с названием задачи
+        taskTitle?.text = taskText
+        
+        // Обновление метки в соответствии с текущим типом
+        taskTypeLabel?.text = taskTitles[taskType]
+    }
 }

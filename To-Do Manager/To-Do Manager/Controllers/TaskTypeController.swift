f1b25decde97219b13098ca489166f4006d37394
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-14 05:11:59 +0300

    TaskType editing
    Adding information about amount of strings and sections in taskType screen.

--- To-Do Manager/To-Do Manager/Controllers/TaskTypeController.swift
+++ To-Do Manager/To-Do Manager/Controllers/TaskTypeController.swift
@@ -3,6 +3,19 @@
 import UIKit
 
 class TaskTypeController: UITableViewController {
+    
+    
+    // 1. Кортеж, описывающий тип задачи
+    typealias TypeCellDescription = (type: TaskPriority, title: String, description: String)
+    
+    // 2. Коллекция доступных типов задач с их описанием
+    private var taskTypesInformation: [TypeCellDescription] = [
+        (type: .important, title: "Важная", description: "Такой тип задач является наиболее приоритетным для выполнения. Все важные задачи выводятся в самом верху списка задач"),
+        (type: .normal, title: "Текущая", description: "Задача с обычным приоритетом")
+    ]
+    
+    // 3. Выбранный приоритет
+    var selectedType: TaskPriority = .normal
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,14 +26,11 @@ class TaskTypeController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return taskTypesInformation.count
     }
-
    
 }

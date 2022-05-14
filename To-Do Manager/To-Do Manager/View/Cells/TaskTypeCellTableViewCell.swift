faa0fd7d57e68c57d314f58b2ba3e49812c7bd69
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-14 04:50:15 +0300

    Adding new xib-file for task types

---
+++ To-Do Manager/To-Do Manager/View/Cells/TaskTypeCellTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TaskTypeCellTableViewCell.swift
+//  To-Do Manager
+//
+//  Created by Арсентий Халимовский on 14.05.2022.
+//
+
+import UIKit
+
+class TaskTypeCellTableViewCell: UITableViewCell {
+    
+    // 1. Кортеж, описывающий тип задачи
+    
+    
+    @IBOutlet var typeTitle: UILabel!
+    @IBOutlet var typeDescription: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

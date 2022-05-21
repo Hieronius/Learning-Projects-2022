e67b75ce696586904bf57fb14bbefe5ea6862eb7
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-21 13:42:50 +0300

    Daily practice with nested func

--- MyPlayground v1.2.playground/Contents.swift
+++ MyPlayground v1.2.playground/Contents.swift
@@ -30,3 +30,44 @@ func sumWallet(banknotesFunction wallet: (Int) -> [Int], walletLenght: Int) -> I
 
 // Передача функции в функцию
 sumWallet(banknotesFunction: generateWallet, walletLenght: 20)
+
+///
+
+func oneStep(coordinates: inout (Int, Int), stepType: String) -> Void {
+    
+    func up(coords: inout (Int, Int)) {
+        coords = (coords.0+1, coords.1)
+    }
+    
+    func right(coords: inout (Int, Int)) {
+        coords = (coords.0, coords.1+1)
+    }
+    
+    func down(coords: inout (Int, Int)) {
+        coords = (coords.0-1, coords.1)
+    }
+    
+    func left(coords: inout (Int, Int)) {
+        coords = (coords.0, coords.1-1)
+    }
+
+
+switch stepType {
+case "up":
+    up(coords: &coordinates)
+case "right":
+    right(coords: &coordinates)
+case "down":
+    down(coords: &coordinates)
+case "left":
+    left(coords: &coordinates)
+default:
+    break
+}
+
+}
+
+var coordinates = (10, -5)
+oneStep(coordinates: &coordinates, stepType: "up")
+oneStep(coordinates: &coordinates, stepType: "right")
+

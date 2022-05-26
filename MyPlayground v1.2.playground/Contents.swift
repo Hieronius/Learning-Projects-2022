f142f6faa4cb27328fcd6ca9792fe4b2aacbca6a
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-26 12:06:08 +0300

    adding operate func
    nested func with addition and subtraction numbers depend of symbol

--- MyPlayground v1.2.playground/Contents.swift
+++ MyPlayground v1.2.playground/Contents.swift
@@ -316,4 +316,57 @@ Goliafh(totalDamage: &totalDamage, typeOfTheAmmo: .frozenAmmo, typeOfTheWeapon: 
 
 
 
+// outer function
+func greetMessage() {
+    
+    // inner function
+    func displayName() {
+        print("Good Morning Abraham!")
+    }
+    
+    // calling inner function
+    displayName()
+}
 
+greetMessage()
+
+
+// nested function with parameters
+
+// outer function
+func addNumbers() {
+    print("Addition")
+    
+    // inner function
+    func display(num1: Int, num2: Int) {
+        print("5 + 10 =", num1 + num2 )
+    }
+    
+    // calling inner function with two values
+    display(num1: 5, num2: 10)
+}
+
+// calling outer function
+addNumbers()
+
+
+// nested function with Return Values
+
+func operate(symbol: String) -> (Int, Int) -> Int {
+    
+    // inner function to add two numbers
+    func add(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    // inner function to substract two numbers
+    func subtract(num1: Int, num2: Int) -> Int {
+        return num1 - num2
+    }
+    
+    let operation = (symbol == "+") ? add: subtract
+    return operation
+}
+
+let operation = operate(symbol: "+")
+let result = operation(8,3)

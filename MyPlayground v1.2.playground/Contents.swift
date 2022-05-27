cfabfcb3053ba00859a11304558be7f5186ecad2
Arsenty Halimovsky <ghost12@yandex.ru>
2022-05-27 13:19:29 +0300

    Daily practice with nested functions

--- MyPlayground v1.2.playground/Contents.swift
+++ MyPlayground v1.2.playground/Contents.swift
@@ -370,3 +370,19 @@ func operate(symbol: String) -> (Int, Int) -> Int {
 
 let operation = operate(symbol: "+")
 let result = operation(8,3)
+
+
+func compare(_ r1: Double, _ r2: Double) {
+    
+    func square(_ r: Double) -> Double {
+        return r * r * 3.14
+    }
+    
+    let s1 = square(r1)
+    let s2 = square(r2)
+    
+    print("разница площадей:", (s1 - s2))
+}
+
+compare(16.0, 15.0)
+

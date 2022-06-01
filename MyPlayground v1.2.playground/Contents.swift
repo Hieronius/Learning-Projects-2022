7d0e8204da59da09226664a25d747361cf09581c
Arsenty Halimovsky <ghost12@yandex.ru>
2022-06-01 12:12:30 +0300

    Daily practice with type properties

--- MyPlayground v1.2.playground/Contents.swift
+++ MyPlayground v1.2.playground/Contents.swift
@@ -1,40 +1,51 @@
+import CoreGraphics
+/// Type property
 
-// War game from Samoilov
+/* Global variables it's a variables that can be used in enywhere in the code
+   Local variables free to use only in inflaence area like func or closure body.
+ 
+   Property of type - it's a special property of class, stuct or enums and can't be blank or without value.
+ 
+   Best idea to incapsulate global variables into the struct and if you need a specific variable just use a dot sintaxis */
 
-class Human {
+struct myGlobalVariables {
     
-    var health: Int
-    var weaponDamage: Int
-    
-    init(health: Int, weaponDamage: Int) {
-        self.health = health
-        self.weaponDamage = weaponDamage
-    }
+    static var firstVar = 1
+    static var secondVar = 2
+    static var thirdVar = 3
 }
 
-class Ork: Human {
-    override var health: Int {
-        didSet {
-            print("Health is not \(health)")
+var myNewArray = [myGlobalVariables.firstVar, myGlobalVariables.secondVar, myGlobalVariables.thirdVar]
+
+var myFirstLocalVar = myGlobalVariables.firstVar
+
+class FirstClass {
+    var arraySortedLeft: [Int]
+    
+    func sorted() -> [Int] {
+        return arraySortedLeft.sorted(by: {$0 < $1} )
+        
+        
+}
+    init(arraySortedLeft: [Int]) {
+        self.arraySortedLeft = arraySortedLeft
+    }
+    
+}
+
+var objectOfFirstClass = FirstClass(arraySortedLeft: myNewArray)
+
+// Let's think about func sorted that want to change my arr variables
+print(var secondNewArray = objectOfFirstClass.sorted()
+
+
+
+class SecondClass {
+    var arraySortedRight: [Int] {
+        get {
+            return arraySortedRight.sorted(by: {$0 > $1 })
         }
     }
-    
-    
 }
 
-class Elf: Human {
-    override var weaponDamage: Int {
-        didSet {
-            print("Weapon damage is increased and now equal \(weaponDamage). Time to buy new armor!")
-        }
-    }
-    
-    
-}
-
-var ork = Ork(health: 100, weaponDamage: 10)
-var elf = Elf(health: 50, weaponDamage: 15)
-
-ork.health = 90
-elf.weaponDamage = 20
-
+var firstClassInstance = FirstClass(arraySortedLeft: myNewArray)

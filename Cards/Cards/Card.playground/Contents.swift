9b6223737baa8fc28fdea6f8db451fbdb7722778
Arsenty Halimovsky <ghost12@yandex.ru>
2022-06-05 05:42:21 +0300

    Added a new custom layer class

--- Cards/Cards/Card.playground/Contents.swift
+++ Cards/Cards/Card.playground/Contents.swift
@@ -15,7 +15,34 @@ extension ShapeLayerProtocol {
 }
 
 
-
+class CircleShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        // рассчитываем данные круга
+        // радиус равен половине меньшей из сторон
+        let radius = ([size.width, size.height].min() ?? 0) / 2
+        
+        // центр круга равен центрам каждый из сторон
+        let centerX = size.width / 2
+        let centerY = size.height / 2
+        
+        // рисуем круг
+        let path = UIBezierPath(arcCenter: CGPoint(x: centerX, y: centerY), radius: radius, startAngle: 0, endAngle: .pi*2, clockwise: true)
+        path.close()
+        
+        // инициализируем созданный путь
+        self.path = path.cgPath
+        
+        // изменяем цвет
+        self.fillColor = fillColor
+    
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
 
 
@@ -23,14 +50,10 @@ class MyViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
-
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
         self.view = view
+        
+        // круг
+        view.layer.addSublayer(CircleShape(size: CGSize(width: 200, height: 150), fillColor: UIColor.gray.cgColor))
     }
 }
 

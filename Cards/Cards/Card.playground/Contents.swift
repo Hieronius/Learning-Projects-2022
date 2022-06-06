19e976fb4aab7e284bc2ae11afeed23cc380d10c
Arsenty Halimovsky <ghost12@yandex.ru>
2022-06-06 05:56:08 +0300

    Made generic class for shapes
    That class can work with any kinds of shapes in my app

--- Cards/Cards/Card.playground/Contents.swift
+++ Cards/Cards/Card.playground/Contents.swift
@@ -15,6 +15,24 @@ extension ShapeLayerProtocol {
 }
 
 
+class CardView<ShapeType: ShapeLayerProtocol>: UIView {
+    
+    // цвет фигуры
+    var color: UIColor!
+    
+    init(frame: CGRect, color: UIColor) {
+        super.init(frame: frame)
+        self.color = color
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+
 class CircleShape: CAShapeLayer, ShapeLayerProtocol {
     
     required init(size: CGSize, fillColor: CGColor) {
@@ -116,6 +134,87 @@ class FillShape: CAShapeLayer, ShapeLayerProtocol {
     }
 }
 
+class BackSideCircle: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        
+        let path = UIBezierPath()
+        
+        // Рисуем 15 кругов
+        for _ in 1...15 {
+            
+            // координаты центра очередного круга
+            let randomX = Int.random(in: 0...Int(size.width))
+            let randomY = Int.random(in: 0...Int(size.height))
+            let center = CGPoint(x: randomX, y: randomY)
+            
+            // смещаем указатель к центру круга
+            path.move(to: center)
+            
+            // определяем случайный радиус
+            let radius = Int.random(in: 5...15)
+            
+            // рисуем круг
+            path.addArc(withCenter: center, radius: CGFloat(radius), startAngle: 0, endAngle: .pi*2, clockwise: true)
+        }
+        
+        // инициализируем созданный путь
+        self.path = path.cgPath
+        
+        // изменяем цвет
+        self.strokeColor = fillColor
+        self.fillColor = fillColor
+        self.lineWidth = 1
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
+
+
+
+class BackSideLine: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+    
+    
+        let path = UIBezierPath()
+        
+        // рисуем 15 линий
+        for _ in 1...15 {
+            
+            // координаты начала очередной линии
+            let randomXStart = Int.random(in: 0...Int(size.width))
+            let randomYStart = Int.random(in: 0...Int(size.height))
+            
+            // координаты конца очередной линии
+            let randomXEnd = Int.random(in: 0...Int(size.width))
+            let randomYEnd = Int.random(in: 0...Int(size.height))
+        
+        
+        // смещаем указатель к началу линии
+        path.move(to: CGPoint(x: randomXStart, y: randomYStart))
+        
+        // рисуем линию
+        path.addLine(to: CGPoint(x: randomXEnd, y: randomYEnd))
+            
+        }
+        
+        // инициализируем созданный путь
+        self.path = path.cgPath
+        
+        // изменяем стиль линий
+        self.strokeColor = fillColor
+        self.lineWidth = 3
+        self.lineCap = .round
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
 
 class MyViewController : UIViewController {

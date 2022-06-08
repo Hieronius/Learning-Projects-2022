96d6a7453afe6bbd3553b96920d65c4a1a0821e3
Arsenty Halimovsky <ghost12@yandex.ru>
2022-06-08 12:28:00 +0300

    Daily practice with methods

--- MyPlayground v1.2.playground/Contents.swift
+++ MyPlayground v1.2.playground/Contents.swift
@@ -5,14 +5,27 @@ class Developer {
     var programmingLanguage: String
     var knownLibraries: [String]
     var knownPatterns: [String]
-    var knownEnglish: Bool
+    var knowEnglish: Bool
     var hasPortfolio: Bool
     
-    init(programmingLanguage: String, knownLibraries: [String], knownPatterns: [String], knownEnglish: Bool, hasPortfolio: Bool) {
+    
+    func printAllProperties() {
+        print("Programming language is \(self.programmingLanguage)")
+        print("Known libraries is \(self.knownLibraries)")
+        print("Knows patterns is \(self.knownPatterns)")
+        print("Person know the english is \(self.knowEnglish)")
+        print("Person has a portfolio is \(self.hasPortfolio)")
+    }
+    
+    init(programmingLanguage: String, knownLibraries: [String], knownPatterns: [String], knowEnglish: Bool, hasPortfolio: Bool) {
         self.programmingLanguage = programmingLanguage
         self.knownLibraries = knownLibraries
         self.knownPatterns = knownPatterns
-        self.knownEnglish = knownEnglish
+        self.knowEnglish = knowEnglish
         self.hasPortfolio = hasPortfolio
     }
 }
+
+var Maxim = Developer(programmingLanguage: "Swift", knownLibraries: ["UIKIt", "SwiftUI"], knownPatterns: ["Singleton", "Fabric"], knowEnglish: true, hasPortfolio: true)
+
+Maxim.printAllProperties()

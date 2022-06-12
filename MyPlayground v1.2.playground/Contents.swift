e881d53ee9c06385f4510e7b53abfeffcb841512
Arsenty Halimovsky <ghost12@yandex.ru>
2022-06-12 17:45:37 +0300

    Daily practice with subscripts

--- MyPlayground v1.2.playground/Contents.swift
+++ MyPlayground v1.2.playground/Contents.swift
@@ -1,116 +1,36 @@
+// Индексы
 
-// 13. Methods
 
-class Developer {
-    var programmingLanguage: String
-    var knownLibraries: [String]
-    var knownPatterns: [String]
-    var knowEnglish: Bool
-    var hasPortfolio: Bool
+typealias Description = (coordinateX: Int, coordinateY: Int, color: String)
+
+
+class Map {
     
     
-    func printAllProperties() {
-        print("Programming language is \(self.programmingLanguage)")
-        print("Known libraries is \(self.knownLibraries)")
-        print("Knows patterns is \(self.knownPatterns)")
-        print("Person know the english is \(self.knowEnglish)")
-        print("Person has a portfolio is \(self.hasPortfolio)")
-    }
     
-    init(programmingLanguage: String, knownLibraries: [String], knownPatterns: [String], knowEnglish: Bool, hasPortfolio: Bool) {
-        self.programmingLanguage = programmingLanguage
-        self.knownLibraries = knownLibraries
-        self.knownPatterns = knownPatterns
-        self.knowEnglish = knowEnglish
-        self.hasPortfolio = hasPortfolio
-    }
+    var mountain: Description = (1, 2, "red")
+    var river: Description = (5, 6, "green")
+    var custle: Description = (10, 11, "black")
+    var wasteland: Description = (15, 16, "blue")
+    var unknownPlace: Description = (20, 21, "yellow")
+    
+
 }
 
-var Maxim = Developer(programmingLanguage: "Swift", knownLibraries: ["UIKIt", "SwiftUI"], knownPatterns: ["Singleton", "Fabric"], knowEnglish: true, hasPortfolio: true)
+var map = Map()
 
-
-
-class HRManager {
-    
-    var arrayOfCandidates = [Developer]()
-    
-    var totalScoreOfWorkInterview = 0
-    
-    
-    func scoreOfCandidat(candidate: Developer) -> Bool {
-        for library in candidate.knownLibraries {
-            if [library] == ["UIKIt"] {
-                print("This person know \(library).It's fine candidat")
-                self.totalScoreOfWorkInterview += 1
-                break
-            } else {
-                print("Not good, he don't know UIKit")
-            }
-        }
-        
-        if candidate.programmingLanguage == "Swift" {
-                print("This person know Swift. Plus to his score")
-                self.totalScoreOfWorkInterview += 1
-            } else {
-                print("Not so good, he don't know Swift")
-            }
-        
-        
-        for pattern in candidate.knownPatterns {
-            if [pattern] == ["Singleton"] || [pattern] == ["Fabric"] {
-                print("This person know \(pattern). Plus one to his scores")
-                self.totalScoreOfWorkInterview += 1
-            } else {
-                print("He don't know patterns")
-            }
-        }
-        
-        if candidate.knowEnglish {
-            print("this guy know english. It's great")
-            self.totalScoreOfWorkInterview += 1
-        } else {
-            print("This guy doesn't know english. It's sad")
-        }
-        
-        if candidate.hasPortfolio {
-            print("This guy done job well. He is a good man")
-            self.totalScoreOfWorkInterview += 1
-        } else {
-            print("This guy doesn't have any projects")
-        }
-        
-        if self.totalScoreOfWorkInterview > 3 {
-            print("We will glad to work with you mr. candidate)")
-            arrayOfCandidates.append(candidate)
-            return true
-        } else {
-            print("Sorry bro but plz prepare a little bit better and come again")
-            return false
+class Hero {
+    var name = "Gendalf"
+    var currentPosition: Description = (50, 50, "purple") {
+        didSet {
+            print("The current position right now is \(currentPosition.coordinateX), \(currentPosition.coordinateY), \(currentPosition.color)")
         }
     }
     
-    func letsTrialTheCandidat(candidat: Developer) -> Bool {
+    func move() {
         
-        var totalScore = 0
+        var newPosition: Description = (0, 0, "")
         
-        var canditateProgrammingLanguage: String
-        var candidateKnownLibraries: [String]
-        var candidateKnownPatterns: [String]
-        var candidateKnownEnglish: Bool
-        var candidateHasPortfolio: Bool
         
-        switch candidat {
-        case candidat.programmingLanguage == "Swift":
-            
-        }
     }
 }
-    
-
-
-var myHRManager = HRManager()
-
-var David = Developer(programmingLanguage: "Java", knownLibraries: ["JavaCaffe"], knownPatterns: ["Data Source"], knowEnglish: false, hasPortfolio: false)
-myHRManager.scoreOfCandidat(candidate: David)
-myHRManager.scoreOfCandidat(candidate: Maxim)
-myHRManager.arrayOfCandidates

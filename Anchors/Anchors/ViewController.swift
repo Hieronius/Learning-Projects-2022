c17dafd108d181764713788fd184fc50d1d77065
Arsenty Halimovsky <ghost12@yandex.ru>
2022-06-22 04:58:44 +0300

    Daily practice with anchors v.1

--- Anchors/Anchors/ViewController.swift
+++ Anchors/Anchors/ViewController.swift
@@ -4,6 +4,102 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // кнопка для запуска/перезапуска игры
+        lazy var startButtonView = getStartButtonView()
+        
+        // button for flipping cards
+        lazy var flipButtonView = getFlippedCardsButton()
+    
+    let rootView = UIView()
+    
+    
+    private func getStartButtonView() -> UIButton {
+            
+            // 1
+            // создаем кнопку
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 50))
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            // 2
+            // изменяем положение кнопки
+            button.center.x = view.center.x
+            
+            // 3
+            // получаем доступ к текущему окну
+            let window = UIApplication.shared.windows[0]
+            
+            // определяем отступ сверху от границ окна до Safe Area
+            let topPadding = window.safeAreaInsets.top
+            
+            // устанавливаем координату Y кнопки в соответствии с отступом
+            button.frame.origin.y = topPadding
+            
+            // 3
+            // Настраиваем внешний вид кнопки
+            
+            // 3.1 устанавливаем текст
+            button.setTitle("Начать игру", for: .normal)
+            
+            // 3.2 устанавливаем цвет текста для обычного (не нажатого) состояния
+            button.setTitleColor(.black, for: .normal)
+            
+            // 3.3 устанавливаем цвет текста для нажатого состояния
+            button.setTitleColor(.gray, for: .highlighted)
+            
+            // 3.4 устанавливаем фоновый цвет
+            button.backgroundColor = .systemGray4
+            
+            // 3.5 скругляем углы
+            button.layer.cornerRadius = 10
+            
+            
+            // button.addAction(UIAction(title: "", handler: { action in print("Button was pressed") }), for: .touchUpInside)
+            
+            return button
+        }
+
+
+    private func getFlippedCardsButton() -> UIButton {
+
+            // 1. Creating a button
+            let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+            
+            // 1.1 Accessing to current window
+            let window = UIApplication.shared.windows[0]
+        
+            
+
+            // 2. Changing position X of the button
+            
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.rightAnchor.constraint(equalTo: rootView.rightAnchor, constant: 20).isActive = true
+            
+            // 2.1 Changing position Y of the button
+            let topPadding = window.safeAreaInsets.top
+            
+            // selecting position of our button below the Safe Area of View
+            button.frame.origin.y = topPadding
+
+            // 3. Making a custom view of our button
+            button.setTitle("Перевернуть", for: .normal)
+            
+            // set color for text in normal statement of button
+            button.setTitleColor(.black, for: .normal)
+            
+            // set color for text in pressed statement of button
+            button.setTitleColor(.gray, for: .highlighted)
+            
+            // set background color of the button
+            button.backgroundColor = .systemGray4
+            
+            // set corner radius value
+            button.layer.cornerRadius = 10
+            
+            return button
+            
+            
+        }
+    
     let view1: UIView = {
         let view = UIView()
         view.backgroundColor = .red
@@ -30,6 +126,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // добавим кнопку на сцену
+                view.addSubview(startButtonView)
+                
+                // adding our new flip button to scene
+                view.addSubview(flipButtonView)
+        
+
+        
         
         view.addSubview(view1)
         view.addSubview(view2)

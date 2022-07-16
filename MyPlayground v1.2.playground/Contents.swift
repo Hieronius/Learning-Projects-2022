86d99629263115775cec3a98895d2b6225af037f
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-16 06:40:12 +0300

    Daily practice with Cod Wars v.2

--- MyPlayground v1.2.playground/Contents.swift
+++ MyPlayground v1.2.playground/Contents.swift
@@ -4,13 +4,8 @@ import Foundation
 import UIKit
 
 
-func boolToWord(_ bool: Bool) -> String {
-    if bool == true {
-        return "Yes"
-    } else {
-        return "No"
-    }
-        
+func greet(_ name: String) -> String {
+  return "Hello, \(name) how are you doing today?"
 }
 
 

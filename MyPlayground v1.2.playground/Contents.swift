686c173efae89b3b09dce3ecb8aef51a359fc6e9
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-17 06:37:19 +0300

    Daily practice with Cod Wars v.2

--- MyPlayground v1.2.playground/Contents.swift
+++ MyPlayground v1.2.playground/Contents.swift
@@ -4,11 +4,23 @@ import Foundation
 import UIKit
 
 
-func checkForFactor(_ base: Int, _ factor: Int) -> Bool {
-    return factor % base == 0
+func quarter(of month: Int) -> Int {
+    switch month {
+    case 1...3:
+        return 1
+    case 4...6:
+        return 2
+    case 7...9:
+        return 3
+    case 10...12:
+        return 4
+    default:
+        return 0
+    }
 }
-checkForFactor(2, 6)
-checkForFactor(3, 10)
+quarter(of: 3)
+quarter(of: 6)
+quarter(of: 9)
 
 typealias Description = (coordinateX: Int, coordinateY: Int, color: String, place: String)
 

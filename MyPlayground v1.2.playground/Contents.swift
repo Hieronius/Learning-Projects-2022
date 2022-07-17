8b92e3292d9e2f0c94e0cc7ebd849ef2d18bec1a
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-17 06:41:31 +0300

    Daily practice with Cod Wars v.3

--- MyPlayground v1.2.playground/Contents.swift
+++ MyPlayground v1.2.playground/Contents.swift
@@ -4,23 +4,14 @@ import Foundation
 import UIKit
 
 
-func quarter(of month: Int) -> Int {
-    switch month {
-    case 1...3:
-        return 1
-    case 4...6:
-        return 2
-    case 7...9:
-        return 3
-    case 10...12:
-        return 4
-    default:
-        return 0
+func summation(_ n: Int) -> Int {
+    var total = 0
+    for number in 1...n {
+        total += number
     }
+    return total
 }
-quarter(of: 3)
-quarter(of: 6)
-quarter(of: 9)
+
 
 typealias Description = (coordinateX: Int, coordinateY: Int, color: String, place: String)
 

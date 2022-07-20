78895bd543e38c1713d7374bee276329efc26254
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-20 09:27:31 +0300

    Daily practice with optionals

--- Playground v1.3.playground/Contents.swift
+++ Playground v1.3.playground/Contents.swift
@@ -1,8 +1,27 @@
 import Foundation
 import UIKit
 
-func numberToString(number: Int) -> String {
-  
-  return "\(number)"
+var a = 2
+Double(a).squareRoot()
+
+func squareOrSquareRoot(_ input: [Int]) -> [Int] {
+    var arrayOfnumbers = [Int]()
+    for number in input {
+        if (Double(number).squareRoot())  == 0 {
+            arrayOfnumbers.append(Int(Double(number).squareRoot()))
+        } else {
+            arrayOfnumbers.append(number * number)
+        }
+    }
+    return arrayOfnumbers
 }
-numberToString(number: 5)
+
+squareOrSquareRoot([4, 3, 9, 7, 2, 1])
+
+let teacher: String? = nil
+var result = teacher ?? "0"
+if let unknownTeacher = teacher {
+    print("\(unknownTeacher)")
+} else {
+    print("I don't know")
+}

a530746ab017c5f878843158803b2776637d94ed
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-20 09:49:37 +0300

    Daily practice with Cod Wars v.2

--- Playground v1.3.playground/Contents.swift
+++ Playground v1.3.playground/Contents.swift
@@ -1,22 +1,11 @@
 import Foundation
 import UIKit
 
-var a = 2
-Double(a).squareRoot()
-
-func squareOrSquareRoot(_ input: [Int]) -> [Int] {
-    var arrayOfnumbers = [Int]()
-    for number in input {
-        if number == Int(Double(number).squareRoot()) * Int(Double(number).squareRoot()){
-            arrayOfnumbers.append(Int(Double(number).squareRoot()))
-        } else {
-            arrayOfnumbers.append(number * number)
-        }
-    }
-    return arrayOfnumbers
+func digitize(_ num:Int) -> [Int] {
+    let string = String(num)
+    return string.compactMap { $0.wholeNumberValue }.reversed()
 }
 
-squareOrSquareRoot([4, 3, 9, 7, 2, 1])
-
+digitize(1231414)
 
 

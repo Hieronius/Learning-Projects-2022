fd2271103fef61059d4e9a75b55db1b8ee86614f
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-22 07:26:56 +0300

    Added outlets for our buttons

---
+++ BirthdayTracker/Controller/AddBirthdayViewController.swift
@@ -0,0 +1,20 @@
+
+
+import UIKit
+
+class AddBirthdayViewController: UIViewController {
+
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    @IBOutlet var birthdayPicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+

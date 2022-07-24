d1649e8eb803bf91ba96d52374bcb50deac07879
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-24 12:02:41 +0300

    Added method to save user names

--- BirthdayTracker/Controller/AddBirthdayViewController.swift
+++ BirthdayTracker/Controller/AddBirthdayViewController.swift
@@ -11,6 +11,16 @@ class AddBirthdayViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        birthdatePicker.maximumDate = Date()
+        
+    }
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        print("Save button pressed")
+        
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        print("My name is \(firstName) \(lastName). ")
     }
 
 

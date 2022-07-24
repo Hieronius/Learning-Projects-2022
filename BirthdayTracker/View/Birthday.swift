bb78430645015ae6c2f5a5f5e95161cfdb3e4eb7
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-24 09:15:07 +0300

    Added class Birthday

---
+++ BirthdayTracker/View/Birthday.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+class Birthday {
+    let firstName: String
+    let lastName: String
+    let birthdate: Date
+    
+    init(firstName: String, lastName: String, birthdate: Date) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.birthdate = birthdate
+    }
+}

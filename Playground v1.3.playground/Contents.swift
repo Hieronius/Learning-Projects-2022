1eeb1164432ea15955ab953004b3585861b838ec
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-28 14:32:38 +0300

    Daily practice with Cod Wars v.2

--- Playground v1.3.playground/Contents.swift
+++ Playground v1.3.playground/Contents.swift
@@ -2,22 +2,19 @@ import Foundation
 import UIKit
 
 
-var a = ""
-var b = ""
-var c = Int(a) ?? 1
-let sum = String((Int(a) ?? 0) + (Int(b) ?? 0))
-
-func sum_str(_ a:String, _ b:String) -> String {
-    if a == a && b == b {
-        let sum = String((Int(a) ?? 0) + (Int(b) ?? 0))
-            return sum
-    } else if a == "" || b == "" {
-        return "0"
+func findDifference(_ a: [Int], _ b: [Int]) -> Int {
+    
+    if a.reduce(1, *) > b.reduce(1, *) {
+        return a.reduce(1, *) - b.reduce(1, *)
     } else {
-      return "0"
+        return b.reduce(1, *) - a.reduce(1, *)
     }
-
+    
+    
 }
 
+findDifference([1,4,4], [3,2,5])
+findDifference([9,7,2], [5,2,2])
 
 
+// return Int(exactly: UInt(a.reduce(1, *)) - UInt(b.reduce(1, *)))!

37e71606cfabd1790606ce984d351023946b66be
Arsenty Halimovsky <ghost12@yandex.ru>
2022-07-30 14:14:16 +0300

    Daily practice with Cod Wars v.1

--- Playground v1.3.playground/Contents.swift
+++ Playground v1.3.playground/Contents.swift
@@ -2,18 +2,42 @@ import Foundation
 import UIKit
 
 
-func multi_table(_ number: Int) -> String {
-    var counter = 2
-    var string = "1 * \(number) = \(number * 1)"
-    
-    for _ in 2...10 {
-        string += "\n\(counter) * \(number) = \(number * counter)"
-        counter += 1
+func warnTheSheep(_ queue: [String]) -> String {
+    if queue.last == "wolf" {
+        return "Pls go away and stop eating my sheep"
     }
-    print(string)
-    return string
+    let wolfPosition = queue.firstIndex(of: "wolf")!
+    return "yo, sheep number \(queue[wolfPosition+1]) , there is the wolf"
 }
 
-multi_table(5)
+warnTheSheep(["sheep", "sheep", "sheep", "sheep", "sheep", "wolf", "sheep", "sheep"])
+warnTheSheep(["sheep", "wolf", "sheep", "sheep", "sheep", "sheep", "sheep"])
+warnTheSheep(["sheep", "sheep", "wolf"])
 
-// almost done but seems like result with \n is wrong. Something simple.
+
+
+
+
+
+
+
+
+func stringy(_ size: Int) -> String {
+
+    var sum1 = ""
+    var sum2 = "1"
+    
+    if size % 2 == 0 {
+        for _ in 1...size/2 {
+            sum1 += "10"
+        }
+        return sum1
+        
+    } else {
+        for _ in 0...size/2 {
+            sum2 += "01"
+        }
+        return sum2
+    }
+    
+}

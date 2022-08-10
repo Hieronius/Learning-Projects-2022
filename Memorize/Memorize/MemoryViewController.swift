6f6900617d2f8356b95fc4a2c648b6ce809880a4
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-10 14:40:26 +0300

    added method to "tap" to open new words

--- Memorize/Memorize/MemoryViewController.swift
+++ Memorize/Memorize/MemoryViewController.swift
@@ -8,9 +8,36 @@ class MemoryViewController: UIViewController {
     
     var item: MemoryItem!
     
+    var blankCounter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        assert(item != nil, "You must provide a memory item before trying to show this view controller")
+        showText()
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(wordsTapped))
+        textView.addGestureRecognizer(tapRecognizer)
+    }
+    
+    func showText() {
+        let words = item.text.components(separatedBy: " ")
+        var output = ""
+        
+        for (index, word) in words.enumerated() {
+            if index < blankCounter {
+                output += "\(word) "
+            } else {
+                let blank = String(repeating: "_", count: word.count)
+                output += "\(blank) "
+            }
+        }
+        
+        textView.text = output
+    }
+    
+    @objc func wordsTapped() {
+        blankCounter += 1
+        showText()
     }
 }

955621ba5db54593a7d77493aa5d792f4a46afa0
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-10 13:56:41 +0300

    Made new MemoryClass

---
+++ Memorize/Memorize/MemoryViewController.swift
@@ -0,0 +1,12 @@
+
+
+import UIKit
+
+class MemoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}

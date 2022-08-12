009d7f334364e0708321e0d2e46584a9b32a7883
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-12 13:05:43 +0300

    Tried to get API link

--- WeatherApp/WeatherApp/ViewController.swift
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchBar.delegate = self
     }
-
-
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let urlString = 
+    }
+}

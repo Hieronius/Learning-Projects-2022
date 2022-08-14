50640d65f69250e04be08724076761df240df600
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-14 20:43:05 +0300

    Creation of new app - VK clone

---
+++ VK/VK/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Арсентий Халимовский on 14.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

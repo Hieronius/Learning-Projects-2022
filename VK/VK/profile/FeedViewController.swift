701da6b2ba7a506acc0d8b5f1c93b1165b3f6154
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-15 11:30:44 +0300

    added view controllers for our tabs

---
+++ VK/VK/profile/FeedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FeedViewController.swift
+//  VK
+//
+//  Created by Арсентий Халимовский on 15.08.2022.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}

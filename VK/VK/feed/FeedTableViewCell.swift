f47a0a53ac75e6e499bb1ac21d135694235a25d3
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-18 20:55:56 +0300

    Added outlets for our posts in code

---
+++ VK/VK/feed/FeedTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  FeedTableViewCell.swift
+//  VK
+//
+//  Created by Арсентий Халимовский on 18.08.2022.
+//
+
+import UIKit
+
+class FeedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postName: UIStackView!
+    
+    @IBOutlet weak var postDate: UILabel!
+    
+    @IBOutlet weak var postText: UILabel!
+    
+    @IBOutlet weak var postImage: UIImageView!
+    
+    
+}

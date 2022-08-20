93d4d648a7a98993edd0120c4f5e4f6b23344fc2
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-20 16:23:45 +0300

    Making a new project "TikTok"

---
+++ TikTok/TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Арсентий Халимовский on 20.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

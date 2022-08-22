387c2d1eef8670f521f6a84966eb64de244858cb
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-22 10:22:59 +0300

    Logic of the app is complete

--- VSCO/VSCO/Editor/EditorViewController.swift
+++ VSCO/VSCO/Editor/EditorViewController.swift
@@ -11,29 +11,43 @@ class EditorViewController: UIViewController {
     
     public var picture: UIImage?
     
+    @IBOutlet weak var filterView: UIView!
+    
+    
     
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.imageView.image = picture
+        filterView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func action1(_ sender: Any) {
+        
+        filterView.backgroundColor = UIColor.Filter.blue
     }
     
     @IBAction func action2(_ sender: Any) {
+        
+        filterView.backgroundColor = UIColor.Filter.grey
     }
     
     @IBAction func action3(_ sender: Any) {
+        
+        filterView.backgroundColor = UIColor.Filter.orange
     }
     
     @IBAction func action4(_ sender: Any) {
+        
+        filterView.backgroundColor = UIColor.Filter.purple
     }
     
     @IBAction func action5(_ sender: Any) {
+        
+        filterView.backgroundColor = UIColor.Filter.yellow
     }
     
     

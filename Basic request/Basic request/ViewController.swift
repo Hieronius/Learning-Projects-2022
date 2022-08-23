ae1a3fdd831f594710f3744e5d6341ea1b223f5a
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-23 09:49:28 +0300

    Made a simple url request from free API

--- Basic request/Basic request/ViewController.swift
+++ Basic request/Basic request/ViewController.swift
@@ -11,13 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeRequest()
         // Do any additional setup after loading the view.
     }
 
     private func makeRequest() {
-        var request = URLRequest(url: URL(string: "https://v2.jokeapi.dev/joke/Any")!)
+        var request = URLRequest(url: URL(string: "https://api.chucknorris.io/jokes/random")!)
         
-        let task = URLSession.shared.dataTask(with: request) {
+        let task = URLSession.shared.dataTask(with: request) {data, respone, error in
+            
+            print(String(decoding: data!, as: UTF8.self))
+            print(error)
+        }
+        task.resume()
     }
 
 }

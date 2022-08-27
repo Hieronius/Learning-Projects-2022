404f8191f8decef2bc7975720cceeb6d3b441228
Arsenty Halimovsky <ghost12@yandex.ru>
2022-08-27 17:02:00 +0300

    Made new project without storyboard

---
+++ RouteTestTask_YouTube/RouteTestTask_YouTube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RouteTestTask_YouTube
+//
+//  Created by Арсентий Халимовский on 26.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

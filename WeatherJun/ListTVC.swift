10a76cd41184a4c676b2c8f826f8c51668103852
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-02 18:17:48 +0300

    Got url from Yandex API

--- WeatherJun/ListTVC.swift
+++ WeatherJun/ListTVC.swift
@@ -7,6 +7,24 @@ class ListTVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    func fatchWeather() {
+        
+        let urlString = "https://api.weather.yandex.ru/v2/forecast?lat=59.932455?lon=30.349285"
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+        request.addValue("ed19aa92-679c-400c-ab68-50387fd0cede", forHTTPHeaderField: "X-Yandex-API-Key")
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            print(String(data: data, encoding: .utf8))
+        }
         
     }
 
@@ -20,6 +38,12 @@ class ListTVC: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        return cell
     }
 
 }

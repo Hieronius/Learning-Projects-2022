cd2dca16787bf2c7332ea9c097afc388d8d8ba53
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-03 19:15:50 +0300

    Got access to "Info", "Fact", "Forecast" from our JSON

---
+++ WeatherJun/Model/WeatherData.swift
@@ -0,0 +1,46 @@
+
+
+import Foundation
+
+struct WeatherData: Decodable {
+    
+    let info: Info
+    let fact: Fact
+    let forecast: [Forecast]
+    
+    
+    
+    
+}
+
+
+struct Info: Decodable {
+    
+    let url: String
+}
+
+struct Fact: Decodable {
+    
+    let temp: Int
+    let icon: String
+    let condition: String
+    let windSpeed: Int
+    let pressureMm: Int
+}
+
+struct Forecast: Decodable {
+    
+    let parts: Parts
+    
+}
+
+struct Parts: Decodable {
+    
+    let day: Day
+}
+
+struct Day: Decodable {
+    
+    let tempMin: Int?
+    let tempMax: Int?
+}

f3127c3bb851ad41232296fdceadfba534b81a1d
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-08 15:08:55 +0300

    Edited parsJSON method

--- WeatherJunSecond/WeatherJunSecond/Model/NetworkWeatherManager.swift
+++ WeatherJunSecond/WeatherJunSecond/Model/NetworkWeatherManager.swift
@@ -5,8 +5,9 @@ struct NetworkWeatherManager {
     
     func fetchWeather() {
         
-        let urlString = "https://api.weather.yandex.ru/v2/forecast?lat=59.932455&lon=30.349285"
-        
+        // let urlString = "https://api.weather.yandex.ru/v2/forecast?lat=59.932455&lon=30.349285"
+        let urlString = "https://api.weather.yandex.ru/v2/forecast?lat=55.751133&lon=37.599310"
+    
         guard let url = URL(string: urlString) else { return }
         
         var request = URLRequest(url: url, timeoutInterval: Double.infinity)
@@ -18,7 +19,10 @@ struct NetworkWeatherManager {
                 print(String(describing: error))
                 return
             }
-            print(String(data: data, encoding: .utf8)!)
+            // print(String(data: data, encoding: .utf8)!)
+            if let weather = self.parseJSON(withData: data) {
+                print(weather)
+            }
         }
         
         task.resume()
@@ -41,7 +45,9 @@ struct NetworkWeatherManager {
             
         } catch let error as NSError {
             
-            print(error.localizedDescription)
+            // print(error.localizedDescription)
+            print(String(describing: error))
+            print("It's won't work")
             
         }
         

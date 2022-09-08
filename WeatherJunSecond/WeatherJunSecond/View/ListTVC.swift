fa41146529f4a2a051cbca91013ede94b9f4f73e
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-08 11:17:09 +0300

    Made correct file system of app

---
+++ WeatherJunSecond/WeatherJunSecond/View/ListTVC.swift
@@ -0,0 +1,54 @@
+
+
+import UIKit
+
+class ListTVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        fetchWeather()
+    }
+    
+    func fetchWeather() {
+        
+        let urlString = "https://api.weather.yandex.ru/v2/forecast?lat=59.932455&lon=30.349285"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+        request.addValue("ed19aa92-679c-400c-ab68-50387fd0cede", forHTTPHeaderField: "X-Yandex-API-Key")
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            print(String(data: data, encoding: .utf8)!)
+        }
+        
+        task.resume()
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        return cell
+    }
+
+}

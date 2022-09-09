ccc1db0a0335dc271b93013afdd705616a6a06a7
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-09 10:29:09 +0300

    Made Weather object for DetailVC

--- WeatherJunSecond/WeatherJunSecond/View/DetailVC.swift
+++ WeatherJunSecond/WeatherJunSecond/View/DetailVC.swift
@@ -15,9 +15,25 @@ class DetailVC: UIViewController {
     @IBOutlet weak var minTempLabel: UILabel!
     @IBOutlet weak var maxTempLabel: UILabel!
     
+    var weatherModel: Weather?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    func refreshLabels() {
+        
+        nameCityLabel.text = weatherModel?.name
+        
+        
+        conditionLabel.text = weatherModel?.conditionString
+        tempCity.text = "\((weatherModel?.temperature)!)"
+        pressureLabel.text = "\((weatherModel?.pressureMm)!)"
+        windSpeedLabel.text = "\((weatherModel?.windSpeed)!)"
+        minTempLabel.text = "\((weatherModel?.tempMin)!)"
+        maxTempLabel.text = "\((weatherModel?.tempMax)!)"
+        
         
     }
     

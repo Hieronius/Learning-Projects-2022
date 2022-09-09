975bbf9534fc3ae9373da8cb0eae01b5494e640d
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-09 09:34:29 +0300

    Made first 3 labels for name and status of tawn

--- WeatherJunSecond/WeatherJunSecond/Model/ListCell.swift
+++ WeatherJunSecond/WeatherJunSecond/Model/ListCell.swift
@@ -4,6 +4,22 @@ import UIKit
 
 class ListCell: UITableViewCell {
 
+    @IBOutlet weak var nameCityLabel: UILabel!
+    @IBOutlet weak var conditionCityLabel: UILabel!
+    @IBOutlet weak var tempCityLabel: UILabel!
     
-
+    func configure(weather: Weather) {
+        
+        self.nameCityLabel.text = weather.name
+        self.conditionCityLabel.text = weather.condition
+        self.tempCityLabel.text = weather.temperatureString
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 }

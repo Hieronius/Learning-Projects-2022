636d064039b45f5382191463a0283ac42ce540de
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-09 18:41:40 +0300

    Started the new project

---
+++ TestMail_Youtube/TestMail_Youtube/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMail_Youtube
+//
+//  Created by Арсентий Халимовский on 09.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

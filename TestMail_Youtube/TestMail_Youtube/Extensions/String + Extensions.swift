d775af32e7e37315b0f73b5b5105c9b3365818b7
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-11 09:51:37 +0300

    Made new extension
    To work with predicats

---
+++ TestMail_Youtube/TestMail_Youtube/Extensions/String + Extensions.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension String {
+    
+    func isValid() -> Bool {
+        
+        let format = "SELF MATCHES %@"
+        let regEx = "[a-aZ-Z0-9._]+@[a-zA-Z]+\\.[a-zA-Z]{2,}"
+        return NSPredicate(format: format, regEx).evaluate(with: self)
+    }
+}

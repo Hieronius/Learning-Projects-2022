2926a70fb231a5a3ca9416d507060912d5bb59d3
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-11 09:58:51 +0300

    Made special color for our textLabel

--- TestMail_Youtube/TestMail_Youtube/Views/VerificationVC/StatusLabel.swift
+++ TestMail_Youtube/TestMail_Youtube/Views/VerificationVC/StatusLabel.swift
@@ -3,6 +3,16 @@ import UIKit
 
 class StatusLabel: UILabel {
     
+    public var isValid = false {
+        didSet {
+            if self.isValid {
+                setValidSetting()
+            } else {
+                setNotValidSetting()
+            }
+        }
+    }
+    
     override init(frame: CGRect) {
         
         super.init(frame: frame)
@@ -19,10 +29,20 @@ class StatusLabel: UILabel {
     private func configure() {
         
         text = "Check your mail"
-        textColor = .white
+        textColor = #colorLiteral(red: 0.9564198852, green: 0.9451850057, blue: 0.8892086744, alpha: 1)
         font = UIFont(name: "Apple SD Gothic Neo", size: 16)
         adjustsFontSizeToFitWidth = true
         translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setNotValidSetting() {
+        text = "Mail is not valid. Example: name@domail.ru"
+        textColor = #colorLiteral(red: 0.5215686275, green: 0.1098039216, blue: 0.05098039216, alpha: 1)
+    }
+    
+    private func setValidSetting() {
+        text = "Mail is valid."
+        textColor = #colorLiteral(red: 0.1960784314, green: 0.3411764706, blue: 0.1019607843, alpha: 1)
     }
 }
 

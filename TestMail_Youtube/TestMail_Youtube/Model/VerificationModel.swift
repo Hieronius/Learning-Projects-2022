7893fdddfe43b03fd6f2b401a8f93dbbf547c7b3
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-11 09:02:12 +0300

    Made new file for Model
    VerificationModel will work with writed text and filter it for popular mail inboxes

---
+++ TestMail_Youtube/TestMail_Youtube/Model/VerificationModel.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class VerificationModel {
+    
+    private let mailsArray = ["@gmail.com", "@yandex.ru", "@yahoo.com"]
+    
+    public var nameMail = String()
+    
+    public var filteredMailArray = [String]()
+    
+    private func 
+}

309be50477a2a65f8f3a6c080f899a0614ddbb11
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-13 14:33:16 +0300

    Add special method for our buttons

--- Project 2/Project 2/ViewController.swift
+++ Project 2/Project 2/ViewController.swift
@@ -16,19 +16,49 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuastion()
     }
     
     func askQuastion() {
+        countries.shuffle() // it's shuffle the array
+        correctAnswer = Int.random(in: 0...2) // it's take random number of element in 0-2 index and set it's as a right answer
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button2.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased() // define title as correct answer.uppercased()
+        // And after new game it will make new random countries and correct answer
     }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+    }
+    
 
 }
 

5916a680491f3ce4f4f034372d4ccb851a475353
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-13 11:24:11 +0300

    Added ability to hide "back" button
    When you choose the image

--- Project 1/Project 1/ViewController.swift
+++ Project 1/Project 1/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        title = "Storm Viewer"
+        
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)

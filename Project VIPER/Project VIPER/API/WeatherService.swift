99a9d67a9ca562937170a9a09eda822dc1e00ae5
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-15 10:47:28 +0300

    Made some services to work with Weather API

--- Project VIPER/Project VIPER/API/WeatherService.swift
+++ Project VIPER/Project VIPER/API/WeatherService.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+
+class WeatherService {
+    func getWeather(completion: @escaping (Int) -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            let temperature = Int.random(in: -30...30)
+            completion(temperature)
+        }
+    }
+}

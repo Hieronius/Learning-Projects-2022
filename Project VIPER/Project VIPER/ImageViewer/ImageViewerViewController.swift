d370c1914222e385e89b3cb6eb2da1e38f6ef8e0
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-16 08:03:54 +0300

    Made some order in project

--- Project VIPER/Project VIPER/ImageViewer/ImageViewerViewController.swift
+++ Project VIPER/Project VIPER/ImageViewer/ImageViewerViewController.swift
@@ -13,10 +13,16 @@ protocol ImageViewerViewProtocol: AnyObject {
 
 class ImageViewerViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    
+    
     // MARK: - Public
+    
     var presenter: ImageViewerPresenterProtocol?
 
+    
+    
     // MARK: - View lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter?.viewDidLoaded()
@@ -25,12 +31,14 @@ class ImageViewerViewController: UIViewController {
 }
 
 // MARK: - Private functions
+
 private extension ImageViewerViewController {
     func initialize() {
     }
 }
 
 // MARK: - ImageViewerViewProtocol
+
 extension ImageViewerViewController: ImageViewerViewProtocol {
     func showImage(image: UIImage?) {
         DispatchQueue.main.async {

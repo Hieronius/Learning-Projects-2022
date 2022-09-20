db3773961cc0bd093e761912a65792af8af9f353
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-20 11:43:03 +0300

    Made method to fill textLabel

--- AlertControllerApp/AlertControllerApp/ViewController.swift
+++ AlertControllerApp/AlertControllerApp/ViewController.swift
@@ -8,20 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var hiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showButton(_ sender: Any) {
-        let alertController = UIAlertController(title: "Error", message: "Ahtung", preferredStyle: .alert)
+        self.hiLabel.text = "Hi, "
+        self.alert(title: "Warning", message: "Write your name", style: .alert)
+    }
+    
+    func alert(title: String, message: String, style: UIAlertController.Style) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
         let action = UIAlertAction(title: "Ok", style: .default) { (action) in
-            let text = alertController.textFields?.first?.text
-            print(text ?? "nil text")
+            let text = alertController.textFields?.first
+            self.hiLabel.text! += (text?.text!)! + "!"
         }
         
-        alertController.addTextField(configurationHandler: nil)
+        alertController.addTextField { (textField) in
+            textField.isSecureTextEntry = true
+        }
+        
         
         alertController.addAction(action)
         self.present(alertController, animated: true, completion: nil)

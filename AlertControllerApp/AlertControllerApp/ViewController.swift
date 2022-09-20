1489634e36458581f6b57e8f84a738388bb15723
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-20 11:26:48 +0300

    Made alert controller with action

--- AlertControllerApp/AlertControllerApp/ViewController.swift
+++ AlertControllerApp/AlertControllerApp/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Error", message: "Ahtung", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default) { (action) in
+            let text = alertController.textFields?.first?.text
+            print(text ?? "nil text")
+        }
+        
+        alertController.addTextField(configurationHandler: nil)
+        
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
 

8272a361616ea73b099dad51d8170fc6b6584ee1
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-21 10:50:28 +0300

    Made some changes to another alert controller

--- Project 4/Project 4/ViewController.swift
+++ Project 4/Project 4/ViewController.swift
@@ -7,7 +7,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
     var progressView: UIProgressView!
-    var websites = ["apple.com", "hackingwithswift.com"]
+    var websites = ["hackingwithswift.com", "radio.net", "google.com"]
+    
+    var secured = true
     
     override func loadView() {
         webView = WKWebView()
@@ -18,14 +20,20 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        let url = URL(string: "https://" + websites[0])!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
         
-        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
         
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        
+        
         
         toolbarItems = [progressButton, spacer, refresh]
         navigationController?.isToolbarHidden = false
@@ -33,32 +41,55 @@ class ViewController: UIViewController, WKNavigationDelegate {
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         
         
-        let url = URL(string: "https://" + websites[0])!
-        webView.load(URLRequest(url: url))
-        webView.allowsBackForwardNavigationGestures = true
+       
     }
         @objc func openTapped() {
             let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-            
             for website in websites {
                 ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
             }
             
-            
+            ac.addAction(UIAlertAction(title: "amazon.com", style: .default, handler: openPage))
             ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
             ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
             present(ac, animated: true)
             
+            
+            
         }
     
     func openPage(action: UIAlertAction) {
-        guard let actionTitle = action.title else { return }
-        guard let url = URL(string: "https://" + actionTitle) else { return }
+        let url = URL(string: "https://" + action.title!)!
         webView.load(URLRequest(url: url))
     }
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    
+                    decisionHandler(.allow)
+                    secured = true
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel)
+        secured = false
+        
+        self.presentedViewController?.dismiss(animated: true)
+        
+        let acOne = UIAlertController(title: "Unsecured", message: "Unsecured", preferredStyle: .alert)
+        acOne.addAction(UIAlertAction(title: "Try again", style: .default))
+        
+        present(acOne, animated: true)
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
@@ -67,18 +98,5 @@ class ViewController: UIViewController, WKNavigationDelegate {
         }
     }
     
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        let url = navigationAction.request.url
-        
-        if let host = url?.host {
-            for website in websites {
-                if host.contains(website) {
-                    decisionHandler(.allow)
-                    return
-                }
-            }
-        }
-        decisionHandler(.cancel)
-    }
 }
 

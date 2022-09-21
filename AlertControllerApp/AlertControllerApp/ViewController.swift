d445f537a00c261677b6be0437b8ebdca0d4d689
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-21 10:50:03 +0300

    Made multiple actions for alert controller

--- AlertControllerApp/AlertControllerApp/ViewController.swift
+++ AlertControllerApp/AlertControllerApp/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
     
     
     @IBOutlet var hiLabel: UILabel!
+    @IBOutlet var yoLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,10 +20,16 @@ class ViewController: UIViewController {
 
     @IBAction func showButton(_ sender: Any) {
         self.hiLabel.text = "Hi, "
-        self.alert(title: "Warning", message: "Write your name", style: .alert)
+        self.alert1(title: "Warning", message: "Write your name", style: .alert)
     }
     
-    func alert(title: String, message: String, style: UIAlertController.Style) {
+    
+    @IBAction func showButton2(_ sender: Any) {
+        self.yoLabel.text = "Yo, "
+        self.alert2(title: "Alert", message: "Write your nick-name", style: .alert)
+    }
+    
+    func alert1(title: String, message: String, style: UIAlertController.Style) {
         let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
         let action = UIAlertAction(title: "Ok", style: .default) { (action) in
             let text = alertController.textFields?.first
@@ -38,5 +45,30 @@ class ViewController: UIViewController {
         self.present(alertController, animated: true, completion: nil)
     }
     
+    func alert2(title: String, message: String, style: UIAlertController.Style) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: style)
+        let action = UIAlertAction(title: "Ok", style: .default) { (action) in
+            let text = alertController.textFields?.first
+            self.yoLabel.text! += (text?.text!)! + "!"
+        }
+        let action2 = UIAlertAction(title: "What", style: .default) { (action) in
+            let text = alertController.textFields?.first
+            self.yoLabel.text! += (text?.text!)! + "?"
+        }
+        
+        let action3 = UIAlertAction(title: "Kill ya", style: .default) { (action) in
+            let text = alertController.textFields?.first
+            self.yoLabel.text! += (text?.text!)! + "@"
+        }
+        
+        alertController.addTextField { (textField) in
+            textField.isSecureTextEntry = true
+        }
+        
+        alertController.addAction(action3)
+        alertController.addAction(action)
+        alertController.addAction(action2)
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 

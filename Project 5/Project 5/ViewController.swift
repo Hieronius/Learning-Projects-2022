79115a6598540fc7be7ab347765635717d4f069d
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-23 09:31:56 +0300

    Started project 5

---
+++ Project 5/Project 5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 5
+//
+//  Created by Арсентий Халимовский on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

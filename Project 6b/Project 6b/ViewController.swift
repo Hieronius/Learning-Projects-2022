d35ed16090b22bf5e64009d15ac8289f755284a2
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-25 11:42:03 +0300

    Started a new project

---
+++ Project 6b/Project 6b/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Project 6b
+//
+//  Created by Арсентий Халимовский on 25.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+

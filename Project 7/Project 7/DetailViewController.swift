ea9d8775915e7ad7fea5f11d434c786debdb691f
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-27 17:14:00 +0300

    Made another view controller for web view

---
+++ Project 7/Project 7/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Project 7
+//
+//  Created by Арсентий Халимовский on 27.09.2022.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webview: WKWebView!
+    var detailItem: Petition?
+    
+    override func loadView() {
+        webview = WKWebView()
+        view = webview
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

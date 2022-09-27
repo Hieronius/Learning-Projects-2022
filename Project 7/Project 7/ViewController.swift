ea40887af8aabc1acb6a64f5da055c50ac198e36
Arsenty Halimovsky <ghost12@yandex.ru>
2022-09-27 13:22:35 +0300

    Started a new project

---
+++ Project 7/Project 7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 7
+//
+//  Created by Арсентий Халимовский on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

dd04cdcc31721814118af3ca70eaae88e0e6b0e1
Arsenty Halimovsky <ghost12@yandex.ru>
2022-10-24 07:19:18 +0300

    Created a new cell and custom class for it

---
+++ CollectionViewApp/CollectionViewApp/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewApp
+//
+//  Created by Арсентий Халимовский on 24.10.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet private weak var countryNameLabel: UILabel!
+    
+    func configure(with countryName: String) {
+        countryNameLabel.text = countryName
+    }
+    
+}
---
+++ CollectionViewApp/CollectionViewApp/CollectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CollectionViewController.swift
+//  CollectionViewApp
+//
+//  Created by Арсентий Халимовский on 24.10.2022.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+    let dataSource: [String] = ["USA", "Brazil", "China", "United Kindom", "Japan", "Mexico", "India"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        var cell = UICollectionViewCell()
+        
+        if let countryCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? CollectionViewCell {
+            
+            countryCell.configure(with: dataSource[indexPath.row])
+            
+            cell = countryCell
+        }
+        
+        return cell
+    }
+}

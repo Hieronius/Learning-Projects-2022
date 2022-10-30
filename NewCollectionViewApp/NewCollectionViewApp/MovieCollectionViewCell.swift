2d0980bc6a51cab5ecf98c27f4e6869a447e529c
Arsenty Halimovsky <ghost12@yandex.ru>
2022-10-30 11:29:43 +0300

    Added special class for collectionViewCell

---
+++ NewCollectionViewApp/NewCollectionViewApp/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  NewCollectionViewApp
+//
+//  Created by Арсентий Халимовский on 30.10.2022.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    
+}

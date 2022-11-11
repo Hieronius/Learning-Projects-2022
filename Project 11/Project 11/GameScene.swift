a61a12ad6e092a1914afb7bf0b11b699f98fb8c0
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-11 11:15:26 +0300

    Added background to our game

--- Project 11/Project 11/GameScene.swift
+++ Project 11/Project 11/GameScene.swift
@@ -11,6 +11,8 @@ import SpriteKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        addChild(background)
     }
 }

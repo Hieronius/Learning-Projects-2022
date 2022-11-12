e3505803ae5bf8adec1846134b72ad9a8fe42728
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-12 11:10:48 +0300

    Replaced box to the ball

--- Project 11/Project 11/GameScene.swift
+++ Project 11/Project 11/GameScene.swift
@@ -16,14 +16,18 @@ class GameScene: SKScene {
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
         
-        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
-        box.position = location
-        addChild(box)
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
     }
 }

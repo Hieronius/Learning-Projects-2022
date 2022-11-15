b10c821953c344f0226a76da04aa5ac34b8cb245
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-15 10:57:39 +0300

    Tried to implement Y obstacles for new balls

--- Project 11/Project 11/GameScene.swift
+++ Project 11/Project 11/GameScene.swift
@@ -66,7 +66,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
-        let location = touch.location(in: self)
+        
+         let location = touch.location(in: self)
+        print(location)
         
         let objects = nodes(at: location)
         

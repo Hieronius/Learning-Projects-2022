834d9b5d1111aacfcfa7ea2437fd8671df532a2f
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-18 08:59:11 +0300

    Created a two new projects

---
+++ Project 12 b/Project 10/Person.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project 10
+//
+//  Created by Арсентий Халимовский on 22.10.2022.
+//
+
+import UIKit
+
+class Person: UICollectionViewCell {
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+    
+}

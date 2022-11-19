8420abc82e296a48945ff251f1459c0de834e26b
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-19 09:56:11 +0300

    Added a few more properties

--- Project 12/Project 12/ViewController.swift
+++ Project 12/Project 12/ViewController.swift
@@ -27,6 +27,15 @@ class ViewController: UIViewController {
         
         let dict = ["Name": "Paul", "Country": "UK"]
         defaults.set(dict, forKey: "SavedDictionary")
+        
+        let savedInteger = defaults.integer(forKey: "Age")
+        let savedBoolian = defaults.bool(forKey: "UseFaceID")
+        
+        let savedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        
+        let savedDictionary = defaults.object(forKey: "SavedDictionary") as? [String: String] ?? [String: String]()
+        
+        let savedArray2 = defaults.array(forKey: <#T##String#>)
     }
 
 

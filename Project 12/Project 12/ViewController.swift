f75d856058ece8ea6e8e2c2854a121b6ce3e8dc3
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-19 09:31:05 +0300

    Created a few properties

--- Project 12/Project 12/ViewController.swift
+++ Project 12/Project 12/ViewController.swift
@@ -14,6 +14,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         let defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UserFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        
+        defaults.set(Date(), forKey: "LastRun")
+        defaults.set("Paul Hadson", forKey: "Name")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Paul", "Country": "UK"]
+        defaults.set(dict, forKey: "SavedDictionary")
     }
 
 

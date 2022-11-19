59965a0619e7c1442cccc35a57ec1003af595f76
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-19 10:03:11 +0300

    Duplicated project 10

---
+++ Project 12a/Project 10/PersonNSObject.swift
@@ -0,0 +1,29 @@
+//
+//  PersonNSObject.swift
+//  Project 10
+//
+//  Created by Арсентий Халимовский on 25.10.2022.
+//
+
+import UIKit
+
+class PersonNSObject: NSObject, NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+        image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(image, forKey: "image")
+    }
+
+}

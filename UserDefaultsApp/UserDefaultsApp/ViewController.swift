4f4e5c0f7325e5898379bc3484c03d888a4cc457
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-21 07:46:44 +0300

    Made method to change background color

--- UserDefaultsApp/UserDefaultsApp/ViewController.swift
+++ UserDefaultsApp/UserDefaultsApp/ViewController.swift
@@ -12,10 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let isOn = UserDefaults.standard.bool(forKey: "switchIsOn")
+        updateBackground(isOn: isOn)
     }
 
 
     @IBAction func switchToggled(_ sender: UISwitch) {
+        updateBackground(isOn: sender.isOn)
+        
+        UserDefaults.standard.setValue(sender.isOn, forKey: "switchIsOn")
+    }
+    
+    private func updateBackground(isOn: Bool) {
+        view.backgroundColor = isOn ? .orange : .darkGray
     }
 }
 

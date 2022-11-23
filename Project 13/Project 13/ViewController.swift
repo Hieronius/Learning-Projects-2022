5aa274e12d83fb592ec19265b3181c33d947975b
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-23 14:24:38 +0300

    Created method to add and edit images

--- Project 13/Project 13/ViewController.swift
+++ Project 13/Project 13/ViewController.swift
@@ -7,15 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     
     @IBOutlet var intensity: UISlider!
     
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
     @IBAction func changeFilter(_ sender: Any) {
@@ -26,5 +38,6 @@ class ViewController: UIViewController {
     
     @IBAction func intensityChanged(_ sender: Any) {
     }
+    
 }
 

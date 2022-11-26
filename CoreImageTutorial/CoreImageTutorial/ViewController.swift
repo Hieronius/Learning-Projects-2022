d28fd8b2455e70f6ffd4e57fdd26150a991dab78
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-26 09:58:17 +0300

    Completed with filter app

--- CoreImageTutorial/CoreImageTutorial/ViewController.swift
+++ CoreImageTutorial/CoreImageTutorial/ViewController.swift
@@ -20,12 +20,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         tempArray = arrayOfFilters
-        let startImage = UIImage(named: "agentsOfShield")
+        let startImage = UIImage(named: "1")
         imageView.image = startImage
     }
 
     @IBAction func filterIt(_ sender: Any) {
+        
+        if tempArray.count == 0 {
+            let startImage = UIImage(named: "1")
+            imageView.image = startImage
+            tempArray = arrayOfFilters
+            tempArray.removeFirst()
+    } else {
+        let fileURL = Bundle.main.url(forResource: "1", withExtension: "png")
+        let startImage = CIImage(contentsOf: fileURL!)
+        
+        let filter = CIFilter(name: tempArray.removeFirst())
+        filter?.setValue(startImage, forKey: kCIInputImageKey)
+        
+        let newImage = UIImage(ciImage: (filter?.outputImage)!)
+        self.imageView.image = newImage
+        
+        
     }
-    
+}
 }
 

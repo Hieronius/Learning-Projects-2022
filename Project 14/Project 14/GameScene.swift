c0f2c8d79f8eb60d20e876cbf70e73140543442d
Arsenty Halimovsky <ghost12@yandex.ru>
2022-11-28 09:05:38 +0300

    Created instance of WhackSlot

--- Project 14/Project 14/GameScene.swift
+++ Project 14/Project 14/GameScene.swift
@@ -9,6 +9,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var slots = [WhackSlot]()
     var gameScore: SKLabelNode!
     
     var score = 0 {
@@ -35,4 +36,11 @@ class GameScene: SKScene {
 //    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 //        <#code#>
 //    }
+    
+    func createSlot(at position: CGPoint) {
+        let slot = WhackSlot()
+        slot.configure(at: position)
+        addChild(slot)
+        slots.append(slot)
+    }
 }
--- Project 14/Project 14/WhackSlot.swift
+++ Project 14/Project 14/WhackSlot.swift
@@ -10,4 +10,10 @@ import UIKit
 
 class WhackSlot: SKNode {
 
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+    }
 }

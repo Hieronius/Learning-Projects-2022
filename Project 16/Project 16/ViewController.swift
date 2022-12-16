e2ea495731845f145de857d7222e5bacc482a807
Arsenty Halimovsky <ghost12@yandex.ru>
2022-12-16 10:54:13 +0300

    Added alert controller
    with capital name and info

--- Project 16/Project 16/ViewController.swift
+++ Project 16/Project 16/ViewController.swift
@@ -48,6 +48,15 @@ class ViewController: UIViewController, MKMapViewDelegate {
         
     }
 
-
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let capital = view.annotation as? Capital else { return }
+        
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
 }
 

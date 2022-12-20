87e2d18b8e4e4ee7a5a823c7be6d4686ef303e06
Arsenty Halimovsky <ghost12@yandex.ru>
2022-12-20 14:34:10 +0300

    Some practice with leetCode

--- Playground v1.3.playground/Contents.swift
+++ Playground v1.3.playground/Contents.swift
@@ -1,16 +1,4 @@
-import UIKit
-import CoreLocation
-import MapKit
+
 import Foundation
 
-func location() {
-    
-    let geocoder = CLGeocoder()
-    
-    func geocodeAddressString(String, completionHandler: @escaping CLGeocodeCompletionHandler) {
-        
-        
-    }
-}
 
-CLGeocoder.geocodeAddressString("Москва")

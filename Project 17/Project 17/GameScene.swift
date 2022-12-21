c81caa4ac90825bdcb0460f055d00c067ec56450
Arsenty Halimovsky <ghost12@yandex.ru>
2022-12-21 12:31:51 +0300

    Added condition for scoring

--- Project 17/Project 17/GameScene.swift
+++ Project 17/Project 17/GameScene.swift
@@ -70,6 +70,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func update(_ currentTime: TimeInterval) {
-        
+        for node in children {
+            if node.position.x < -300 {
+                node.removeFromParent()
+            }
+        }
+        if !isGameOver {
+            score += 1
+        }
     }
 }

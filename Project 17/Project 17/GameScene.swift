067c06656586efff31bcbd9d9a9e9b73e15ecee7
Arsenty Halimovsky <ghost12@yandex.ru>
2022-12-21 12:11:25 +0300

    Added field, play and score nodes

--- Project 17/Project 17/GameScene.swift
+++ Project 17/Project 17/GameScene.swift
@@ -22,7 +22,25 @@ class GameScene: SKScene {
     
     
     override func didMove(to view: SKView) {
-        <#code#>
+        backgroundColor = .black
+        
+        starfield = SKEmitterNode(fileNamed: "starfield")!
+        starfield.position = CGPoint(x: 1024, y: 384) // half of the normal Y-size
+        starfield.advanceSimulationTime(10)
+        addChild(starfield)
+        starfield.zPosition = -1
+        
+        player = SKSpriteNode(imageNamed: "player")
+        player.position = CGPoint(x: 100, y: 384) // again only half of the y-size
+        player.physicsBody = SKPhysicsBody(texture: player.texture!, size: player.size)
+        player.physicsBody?.contactTestBitMask = 1
+        addChild(player)
+        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.position = CGPoint(x: 16, y: 16)
+        scoreLabel.horizontalAlignmentMode = .left
+        addChild(scoreLabel)
+        
     }
     
     override func update(_ currentTime: TimeInterval) {

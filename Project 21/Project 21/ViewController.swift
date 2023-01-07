f1389819e582e5c520ff9679ece3dae02eeef956
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-07 10:38:22 +0300

    Started a new project

---
+++ Project 21/Project 21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 21
+//
+//  Created by Арсентий Халимовский on 07.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

e558174d366a9221f9fadbbfab1e4210eb859c0e
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-07 10:51:04 +0300

    Created methods for buttons

--- Project 21/Project 21/ViewController.swift
+++ Project 21/Project 21/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Арсентий Халимовский on 07.01.2023.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
@@ -17,11 +18,30 @@ class ViewController: UIViewController {
     }
 
     @objc func registerLocal() {
-        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("Yay!")
+            } else {
+                print("D'oh!")
+            }
+        }
     }
     
     @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
         
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["customData": "fizzbuzz"]
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
     }
 }
 

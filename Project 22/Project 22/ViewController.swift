8c7644d0c509e441f057085bd9ed50397828523f
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-10 20:30:51 +0300

    implemented core location manager

--- Project 22/Project 22/ViewController.swift
+++ Project 22/Project 22/ViewController.swift
@@ -5,13 +5,25 @@
 //  Created by Арсентий Халимовский on 10.01.2023.
 //
 
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
     }
 
 

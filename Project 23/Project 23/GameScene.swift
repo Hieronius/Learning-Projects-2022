ef0bc38688a3d01a1a0ba032b8959120477bb433
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-12 14:02:23 +0300

    Added background and score method

--- Project 23/Project 23/GameScene.swift
+++ Project 23/Project 23/GameScene.swift
@@ -10,9 +10,46 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var gameScore: SKLabelNode!
+    
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
         
+        // speed of the objects in the scene
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
         
 }
+    
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        
+        gameScore.position = CGPoint(x: 8, y: 8)
+        score = 0
+    }
+    
+    func createLives() {
+        
+    }
+    
+    func createSlices() {
+        
+    }
 }

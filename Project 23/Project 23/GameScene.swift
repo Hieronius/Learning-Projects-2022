b9cdd55f9c88a107c4c8d10d5a196c1bb7f81229
Arsenty Halimovsky <ghost12@yandex.ru>
2023-01-14 12:48:29 +0300

    Another code for touchesMoved

--- Project 23/Project 23/GameScene.swift
+++ Project 23/Project 23/GameScene.swift
@@ -154,9 +154,37 @@ class GameScene: SKScene {
                 
             } else if node.name == "bomb" {
                 // destroy the bomb
+                guard let bombContainer = node.parent as? SKSpriteNode else {
+                    continue }
+                
+                if let emitter = SKEmitterNode(fileNamed: "sliceHitBomb") {
+                    emitter.position = bombContainer.position
+                    addChild(emitter)
+                }
+                
+                node.name = ""
+                bombContainer.physicsBody?.isDynamic = false
+                
+                let scaleOut = SKAction.scale(by: 0.001, duration: 0.2)
+                let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+                let group = SKAction.group([scaleOut, fadeOut])
+                
+                let seq = SKAction.sequence([group, .removeFromParent()])
+                bombContainer.run(seq)
+                
+                if let index = activeEnemies.firstIndex(of: bombContainer) {
+                    activeEnemies.remove(at: index)
+                }
+                
+                run(SKAction.playSoundFileNamed("explosion.caf", waitForCompletion: false))
+                endGame()
             }
                 
         }
+    }
+    
+    func endGame() {
+        
     }
     
     func playSwooshSound() {
